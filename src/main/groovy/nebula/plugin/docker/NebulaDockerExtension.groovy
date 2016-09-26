750229e31136af3fad6d78f63bc2b792c83a5e2f
Liviu Tudor <ltudor@netflix.com>
2016-09-26 12:25:03 -0700

    groovydoc update

--- src/main/groovy/nebula/plugin/docker/NebulaDockerExtension.groovy
+++ src/main/groovy/nebula/plugin/docker/NebulaDockerExtension.groovy
@@ -22,16 +22,80 @@ package nebula.plugin.docker
  * @author ltudor
  */
 class NebulaDockerExtension {
+    /**
+     * Email address of the maintainer of the docker image.
+     */
     def String maintainerEmail
+
+    /**
+     * Map of environment to docker repository.
+     * This allows for the docker image to be deployed in different environments / repos.
+     * For instance, if you have a different docker repo for 'test' and 'prod' you can define this as:
+     * <code>project.nebulaDocker.dockerRepo = [test: 'http://url.for.test/path', prod: 'https://url.prod:port/path']</code>
+     * Note that the keys of this map are used to generate the {@link #getEnvironments()} property.
+     *
+     * @see #getEnvironments()
+     */
     def Map<String, String> dockerRepo
+
+    /**
+     * Docker daemon url.
+     */
     def String dockerUrl
+
+    /**
+     * Docker base image.
+     */
     def String dockerBase
+
+    /**
+     * Where in the project build structure is the Dockerfile.
+     * Typically this will be in <code>./build/docker/Dockerfile</code>, but if you handcrafted your own Dockerfile, specify the path here.
+     */
     def String dockerFile
+
+    /**
+     * Name of the directory to be created in the docker image to host the app.
+     * Typically this is set to be <code>/applicationName-version.version</code> but if you want a different dir structure, specify it here.
+     * Note that {@link #appDirLatest} will be symlink'd to this location.
+     *
+     * @see #appDirLatest
+     */
     def String appDir
+
+    /**
+     * Name of the "latest" dir.
+     * This will be symlink'd to the {@link #appDir} directory in the docker image and typically is set to
+     * <code>/applicationName-latest</code> but if you want to change that name you can do so here.
+     *
+     * @see #appDir
+     */
     def String appDirLatest
+
+    /**
+     * Closure to execute when building the docker image.
+     * By default the code just creates the {@link #appDir} directory and symlinks {@link #appDirLatest} to it and sets the entry point
+     * to the shell script in {@link #appDirLatest}/bin.
+     * If you need any other files or symlinks or commands to be executed, specify them here.
+     * If not set (or set to <code>null</code>) then no extra commands will be added to the Dockerfile.
+     */
     def Closure dockerImage
+
+    /**
+     * Closure used to set the tag on the docker image.
+     * Typically the code will set 2 tags: one with the application version and one with <code>latest</code>.
+     * This closure allows you to define the tagging for the applicaiton version.
+     * If not set, the application version will be set, as per above.
+     */
     def Closure<String> tagVersion
 
+    /**
+     * Defines a property which returns all the environments defined in the {@link #dockerRepo}.
+     * This is basically a set of all the keys present in the {@link #dockerRepo} set.
+     *
+     * @return Immutable set of all the environments present in {@link #dockerRepo} or <code>null</code> if the {@link #dockerRepo} is null/empty.
+     * @see #dockerRepo
+     */
     Set<String> getEnvironments() {
         if (!dockerRepo) return null
         Collections.unmodifiableSet(dockerRepo.keySet())
