df18fc56c2b46cd0d628d612b5fba3471dfbde5a
Liviu Tudor <liviu.tudor@gmail.com>
2018-03-01 08:53:24 -0800

    added parentOf method in preparation of adding support for appDir changes

--- src/test/groovy/nebula/plugin/docker/StringsTest.groovy
+++ src/test/groovy/nebula/plugin/docker/StringsTest.groovy
@@ -51,4 +51,49 @@ class StringsTest extends Specification {
         r3 == "123"
         r4 == "One"
     }
+
+    def "parentOf returns null/empty for null/empty inputs"() {
+        def x = new Object() as Strings
+
+        when:
+        def r1 = x.parentOf(null)
+        def r2 = x.parentOf("")
+        def r3 = x.parentOf('')
+
+        then:
+        notThrown(Throwable)
+        r1 == null
+        r2 == ''
+        r3 == ''
+    }
+
+    def "parentOf returns the directory itself if no parent found"() {
+        def x = new Object() as Strings
+
+        when:
+        def r1 = x.parentOf("/")
+        def r2 = x.parentOf("xyz")
+
+        then:
+        r1 == "/"
+        r2 == "xyz"
+    }
+
+    def "parentOf returns the parent directory"() {
+        def x = new Object() as Strings
+
+        when:
+        def r1 = x.parentOf("/abc")
+        def r2 = x.parentOf("/ab/")
+        def r3 = x.parentOf("/ab/c")
+        def r4 = x.parentOf("/ab/c/")
+        def r5 = x.parentOf("/ab/c/d/e.png")
+
+        then:
+        r1 == "/"
+        r2 == "/"
+        r3 == "/ab"
+        r4 == "/ab"
+        r5 == "/ab/c/d"
+    }
 }
